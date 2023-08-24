58a23ff50849f08fbbb8e335ef646616c3550ee4
Jhumel Bonganciso <theodinprojectjhumel@gmail.com>
2023-08-24 18:46:26 +0800

    adding initial functions

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log("test")
+let numbers = {
+    firstNum: 0,
+    secondNum: 0
+}
+
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 + num2;
+}
+
+function operate(operation) {
+    switch(operation) {
+        case "add":
+            add(numbers.firstNum, numbers.secondNum);
+            break;
+        case "subtract":
+            subtract(numbers.firstNum, numbers.secondNum);
+            break;
+        case "multiply":
+            multiply(numbers.firstNum, numbers.secondNum);
+            break;
+        case "divide":
+            divide(numbers.firstNum, numbers.secondNum);
+    }
+}
