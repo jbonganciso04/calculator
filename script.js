3e9f0929cf57ffeb4b3a558d02e1ef7baa3fd97d
Jhumel Bonganciso <theodinprojectjhumel@gmail.com>
2023-08-28 21:48:29 +0800

    rewrite the whole logic of the application

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
-let numbers = {
-    firstNum: 0,
-    secondNum: 0
-}
-
-let operator = "";
-let temp = "";
-let temp2 = "";
-let result = 0;
-
-const operandTwo = document.getElementById("calculator__screen__firstNum");
+const operandTwoDisplay = document.getElementById("calculator__screen__firstNum");
 const operandOne = document.getElementById("calculator__screen__secondNum");
-const clearBtn = document.getElementById("clear__btn");
 const numbersCollection = document.getElementsByClassName("number");
 const operationCollection = document.getElementsByClassName("operation__symbol");
 const equalSign = document.getElementById("equals__sign");
 
-clearBtn.addEventListener("click", () => {
-    
-})
-
+let temp = "";
+let operation = "";
+let operands = {
+    firstNum: 0,
+    secondNum: 0
+}
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -71,37 +62,32 @@ function getNumberValue(numbersCollection) {
 function getOperationSymbol(operationCollection) {
     for(let i = 0; i < operationCollection.length; i++) {
         operationCollection[i].addEventListener("click", () => {
-           if(operator == "") {
-            operator = operationCollection[i].value;
-            numbers.firstNum = Number(temp);
-            temp2 = temp.concat(` ${operationCollection[i].textContent.trim()}`)
-            operandTwo.textContent = temp2
+            operandTwoDisplay.textContent = temp;
+            operands.firstNum = Number(operandTwoDisplay.textContent);
             temp = "";
-            operandOne.textContent = temp;
-           } else {
-            operator = operationCollection[i].value;
-            replaceCharSymbolAtTemp(temp2, operationCollection[i].textContent)
-           }
+            if(operandTwoDisplay.textContent.includes("+") || operandTwoDisplay.textContent.includes("-") || operandTwoDisplay.textContent.includes("*") || operandTwoDisplay.textContent.includes("÷")) {
+                operandTwoDisplay.textContent = operandTwoDisplay.textContent.replace(operandTwoDisplay.textContent.charAt(operandTwoDisplay.textContent.length - 1), newSymbol.trim());
+                operation = operationCollection[i].value;
+            } else {
+                temp = `${operationCollection[i].textContent}`
+                temp = temp.trim();
+                operandTwoDisplay.textContent = operandTwoDisplay.textContent.concat(" ", temp)
+                temp = "";
+                operandOne.textContent = temp;
+                operation = operationCollection[i].value;
+            }
         })
     }
 }
 
-function replaceCharSymbolAtTemp(temp2, newSymbol) { 
-    if(temp2.includes("+") || temp2.includes("-") || temp2.includes("*") || temp2.includes("÷")) {
-        temp2 = temp2.replace(temp2.charAt(temp2.length - 1), newSymbol.trim());
-        operandTwo.textContent = temp2
-    }
-}
-
 equalSign.addEventListener("click", () => {
-    numbers.secondNum = Number(operandOne.textContent);
-    let result = operate(numbers.firstNum, numbers.secondNum, operator);
-    console.log(result)
-    temp2 = result;
-    operandTwo.textContent = temp2.toString();
-    temp = "";
+    operands.secondNum = Number(operandOne.textContent);
+    let result = operate(operands.firstNum, operands.secondNum, operation);
+    operandTwoDisplay.textContent = (result.toString());
+    temp = ""
     operandOne.textContent = temp
 })
 
-getNumberValue(numbersCollection)
+
+getNumberValue(numbersCollection);
 getOperationSymbol(operationCollection);
