164832ab3bbd66a0839f2ebb89444924f28aaa7b
Jhumel Bonganciso <theodinprojectjhumel@gmail.com>
2023-08-28 17:34:01 +0800

    Added logic to change operation symbol whenever user clicks on different symbol

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let numbers = {
-    firstNum: 0,
-    secondNum: 0
+    firstNum: null,
+    secondNum: null
 }
 
-let operationSymbol = "";
+let operator = "";
+let temp = "";
 
 
-const firstNumber = document.getElementById("calculator__screen__firstNum");
-const secondNumber = document.getElementById("calculator__screen__secondNum");
+const operandTwo = document.getElementById("calculator__screen__firstNum");
+const operandOne = document.getElementById("calculator__screen__secondNum");
 const clearBtn = document.getElementById("clear__btn");
 const numbersCollection = document.getElementsByClassName("number");
-firstNumber.textContent = numbers.firstNum
-secondNumber.textContent = numbers.secondNum
+const operationCollection = document.getElementsByClassName("operation__symbol");
 
 clearBtn.addEventListener("click", () => {
     
@@ -53,8 +53,35 @@ function getNumberValue(numbersCollection) {
     for(let i = 0; i < numbersCollection.length; i++) {
         numbersCollection[i].addEventListener("click", () => {
             console.log(numbersCollection[i].value);
+            temp = temp.concat(numbersCollection[i].value);
+            operandOne.textContent = temp;
         })
     }
 }
 
+function getOperationSymbol(operationCollection) {
+    for(let i = 0; i < operationCollection.length; i++) {
+        operationCollection[i].addEventListener("click", () => {
+           if(operator == "") {
+            operator = operationCollection[i].value;
+            temp = temp.concat(` ${operationCollection[i].textContent.trim()}`)
+            operandTwo.textContent = temp
+            operandOne.textContent = ""
+           } else {
+            operator = operationCollection[i].value;
+            replaceCharSymbolAtTemp(temp, operationCollection[i].textContent)
+           }
+        })
+    }
+}
+
+function replaceCharSymbolAtTemp(temp, newSymbol) { 
+    if(temp.includes("+") || temp.includes("-") || temp.includes("*") || temp.includes("÷")) {
+        temp = temp.replace(temp.charAt(temp.length - 1), newSymbol.trim());
+        console.log("New temp is "+temp);
+        operandTwo.textContent = temp
+    }
+}
+
 getNumberValue(numbersCollection)
+getOperationSymbol(operationCollection);
